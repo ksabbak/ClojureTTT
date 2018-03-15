035d6ef3ff57ad0c34afd6d88539fa61dc5228d5
ksabbak <ksabbak@gmail.com>
2018-03-15 11:45:33 -0500

    Write continue and blank board display

--- src/clojure_tictactoe/core.clj
+++ src/clojure_tictactoe/core.clj
@@ -3,4 +3,6 @@
 
 (defn -main
   [& args]
-  (ttt/intro-game))
+  (ttt/intro-game)
+  (ttt/continue-to-game)
+  (println (ttt/board [0 1 2 3 4 5 6 7 8])))
--- test/clojure_tictactoe/tictactoe_test.clj
+++ test/clojure_tictactoe/tictactoe_test.clj
@@ -6,3 +6,7 @@
 (deftest intro-game-message-test 
   (testing "Intro messages"
     (is (string/includes? (with-out-str (intro-game)) "TicTacToe"))))
+
+(deftest board-test
+  (testing "Displays a simple blank board"
+    (is (= " 0 | 1 | 2 \n===+===+===\n 3 | 4 | 5 \n===+===+===\n 6 | 7 | 8 \n" (board [0 1 2 3 4 5 6 7 8])))))
