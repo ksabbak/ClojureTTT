a58b238479e65c7db7097fa280aadf955adac584
ksabbak <ksabbak@gmail.com>
2018-03-30 15:25:48 -0500

    Clean up

---
+++ test/clojure_tictactoe/cli/output/end_game_printer_test.clj
@@ -0,0 +1,20 @@
+(ns clojure-tictactoe.cli.output.end-game-printer-test
+  (:require [clojure.test :refer :all]
+            [clojure.string :as string]
+            [clojure-tictactoe.helpers :as helper]
+            [clojure-tictactoe.cli.output.end-game-printer :refer :all]))
+
+(testing "End game messaging"
+  (testing "print-game-over"
+    (deftest print-game-over-test-tie
+      (testing "Prints tie message when it gets nil as an argument"
+        (is (string/includes? (with-out-str (print-game-over nil)) game-tie-message))))
+
+    (deftest print-game-over-test-win
+      (testing "Prints won message when it gets a truthy argument"
+        (is (string/includes? (with-out-str (print-game-over "x")) (game-won-message "x"))))))
+
+  (testing "game-won-message"
+    (deftest game-won-message-test
+      (testing "Prints the appropriate message"
+        (is (string/includes? (game-won-message "x") "x!"))))))
